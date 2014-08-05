80e2a0772187f3fc7dcf7a24fd578d647fcce4b6
camdenb <doctorshmoogle@gmail.com>
2014-08-04 17:50:18 -0700

    initial commit

---
+++ header.php
@@ -0,0 +1,5 @@
+<div class='header'>
+	<div class='header-wrapper'>
+		<span class='header-title'>camden bickel</span>
+	</div>
+</div>
---
+++ nav.php
@@ -0,0 +1,13 @@
+<nav class='main-nav'>
+	<ul>
+		<a href='#'><li>
+			about
+		</li></a>
+		<a href='#'><li>
+			portfolio 
+		</li></a>
+		<a href='#'><li>
+			contact me 
+		</li></a>
+	</ul>
+</nav>
