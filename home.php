651a47f436a4c1ef4efbe8986876a2ea416afa78
camdenb <doctorshmoogle+github@gmail.com>
2014-08-04 18:35:36 -0700

    cleaned up nav, fixed title resizing

--- home.php
+++ home.php
@@ -1,5 +1,6 @@
+
 <div class='content'>
-	<h1>HEY WHEN YOU GET BACK: upload old portfolio to github, and continue using git for this project</h1>
+	<h1>i am cammy</h1>
 	<p>Hey, welcome to my site.
 
 		Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc sed ligula non nisi fermentum sollicitudin. Nullam imperdiet tincidunt enim id condimentum. Aenean eget placerat velit, nec convallis lorem. Ut sodales nisl nibh, in gravida nisl viverra eu. Mauris lectus ipsum, cursus id nulla sit amet, dapibus mattis tellus. Vestibulum id imperdiet massa. Fusce feugiat dapibus magna, a fringilla dui vulputate eget. Aenean feugiat, mi et fringilla adipiscing, ante ligula tempor mauris, vitae convallis sem nulla et nunc. Nullam eu lacinia elit. Curabitur venenatis mi id arcu sagittis, eget varius diam pellentesque. Morbi sit amet nulla vitae erat semper ullamcorper. Nunc odio sapien, consequat in libero sit amet, molestie congue eros.
